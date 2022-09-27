23868c2f64492872802f2c9817f8badfede503dd
Rustam <rustamdvl@yandex.ru>
2022-09-27 18:10:13 +0500

    Initial commit

---
+++ Hanoi/MyCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Threading;
+
+namespace WPF1  //  http://www.codearsenal.net/2012/06/wpf-animation-tower-of-hanoi.html
+{
+    public class MyCanvas : Canvas
+    {
+        public int Size { get; set; }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * C# Program to Demonstrate Tower Of Hanoi
+ */
+
+namespace Tests;
+
+static class Program
+{
+    private static int _count;
+    class TowerOfHanoi
+    {
+        int _mNumdiscs;
+        public TowerOfHanoi()
+        {
+            Numdiscs = 0;
+        }
+        public TowerOfHanoi(int newval)
+        {
+            Numdiscs = newval;
+        }
+        public int Numdiscs
+        {
+            get => _mNumdiscs;
+            set
+            {
+                if (value > 0)
+                    _mNumdiscs = value;
+            }
+        }
+        public static void Movetower(int n, int from, int to, int other)
+        {
+            if (n > 0)
+            {
+                Movetower(n - 1, from, other, to);
+                Console.WriteLine("Move disk {0} from tower {1} to tower {2}", 
+                    n, from, to);
+                _count++;
+                Movetower(n - 1, other, to, from);
+            }
+        }
+    }
+    
+    static class TowersOfHanoiApp
+    {
+        public static void Main()
+        {
+            TowerOfHanoi T = new TowerOfHanoi();
+            Console.Write("Enter the number of discs: ");
+            T.Numdiscs = Convert.ToInt32(8);
+            TowerOfHanoi.Movetower(T.Numdiscs, 1, 3, 2);
+            Console.WriteLine(_count);
+        }
+    }
+}
