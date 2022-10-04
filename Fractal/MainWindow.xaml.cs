91a8571f429903c049e4329f96684caf22a216fa
Rustam <rustamdvl@yandex.ru>
2022-10-04 17:12:14 +0500

    commit

--- Fractal/MainWindow.xaml.cs
+++ Fractal/MainWindow.xaml.cs
@@ -4,62 +4,66 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
-namespace Fractal
+namespace Fractal;
+
+public partial class MainWindow
 {
-    public partial class MainWindow
+    private int _depth;
+    private int _i;
+    private const double LengthScale = 0.8;
+    private const double DeltaTheta = Math.PI / 8;
+    public MainWindow()
     {
-        private int _depth;
-        private int _i;
-        private const double LengthScale = 0.75;
-        private const double DeltaTheta = Math.PI / 6;
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void btnStart_Click(object sender, RoutedEventArgs e)
-        {
-            Canvas1.Children.Clear();
-            TbLabel.Text = "";
-            _i = 0;
-            _depth = 1;
-            CompositionTarget.Rendering += StartAnimation!;
-        }
+    private void btnStart_Click(object sender, RoutedEventArgs e)
+    {
+        Canvas1.Children.Clear();
+        TbLabel.Text = "";
+        _i = 0;
+        _depth = 1;
+        CompositionTarget.Rendering += StartAnimation!;
+    }
 
-        private void StartAnimation(object sender, EventArgs e)
+    private void StartAnimation(object sender, EventArgs e)
+    {
+        _i += 1;
+        if (_i % 60 == 0)
         {
-            _i += 1;
-            if (_i % 60 == 0)
+            DrawFractalTree(Canvas1, _depth, new Point(Canvas1.Width / 2, 0.85 * Canvas1.Height), 0.2 * Canvas1.Width,
+                -Math.PI / 2);
+            string str = "Количество шагов = " + _depth;
+            TbLabel.Text = str;
+            _depth += 1;
+            if (_depth >= 15)
             {
-                DrawBinaryTree(Canvas1, _depth, new Point(Canvas1.Width / 2, 0.85 * Canvas1.Height), 0.2 * Canvas1.Width,
-                    -Math.PI / 2);
-                string str = "Количество шагов = " + _depth;
-                TbLabel.Text = str;
-                _depth += 1;
-                if (_depth >= 15)
-                {
-                    TbLabel.Text = $"Количество шагов = {_depth}. Дерево выращено!";
-                    CompositionTarget.Rendering -= StartAnimation!;
-                }
+                TbLabel.Text = $"Количество шагов = {_depth}. Дерево выращено!";
+                CompositionTarget.Rendering -= StartAnimation!;
             }
         }
+    }
 
-        private void DrawBinaryTree(Canvas canvas, int depth, Point pt, double length, double theta)
+    private void DrawFractalTree(Canvas canvas, int depth, Point pt, double length, double theta)
+    {
+        double x2 = pt.X + length * Math.Cos(theta);
+        double y2 = pt.Y + length * Math.Sin(theta);
+        Line line = new Line
         {
-            double x1 = pt.X + length * Math.Cos(theta);
-            double y1 = pt.Y + length * Math.Sin(theta);
-            Line line = new Line
-            {
-                Stroke = Brushes.Azure,
-                X1 = pt.X,
-                Y1 = pt.Y,
-                X2 = x1,
-                Y2 = y1
-            };
-            canvas.Children.Add(line);
-            if (depth == 1) return;
-            DrawBinaryTree(canvas, depth - 1, new Point(x1, y1), length * LengthScale, theta + DeltaTheta);
-            DrawBinaryTree(canvas, depth - 1, new Point(x1, y1), length * LengthScale, theta - DeltaTheta);
-        }
+            Stroke = Brushes.Azure,
+            X1 = pt.X,
+            Y1 = pt.Y,
+            X2 = x2,
+            Y2 = y2
+        };
+        
+        canvas.Children.Add(line);
+        
+        if (depth == 1) return;
+        
+        DrawFractalTree(canvas, depth - 1, new Point(x2, y2), 
+            length * LengthScale, theta + DeltaTheta);
+        DrawFractalTree(canvas, depth - 1, new Point(x2, y2), 
+            length * LengthScale, theta - DeltaTheta);
     }
 }
--- Hanoi/Animation.xaml.cs
+++ Hanoi/Animation.xaml.cs
@@ -6,158 +6,156 @@ using System.Windows.Controls;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
-namespace Hanoi
+namespace Hanoi;
+
+public partial class Animation 
 {
-    public partial class Animation 
+    readonly int _ringsCount;
+    readonly List<Tuple<int, int>> _movementsList = new();
+    public Animation(HelpClass help)
     {
-        readonly int _ringsCount;
-        readonly List<Tuple<int, int>> _movementsList = new();
-        public Animation(HelpClass help)
-        {
-            InitializeComponent();
-            _ringsCount = help.RingsCount;
-            Start();
-        }
-        private void CreateField()
-        {
-            Col1.Children.Clear();
-            Col2.Children.Clear();
-            Col3.Children.Clear();
+        InitializeComponent();
+        _ringsCount = help.RingsCount;
+        Start();
+    }
+    private void CreateField()
+    {
+        Col1.Children.Clear();
+        Col2.Children.Clear();
+        Col3.Children.Clear();
 
-            int ringWidth = HelpClass.RingMinWidth;
-            for (int i = 0; i < _ringsCount; i++)
+        int ringWidth = HelpClass.RingMinWidth;
+        for (int i = 0; i < _ringsCount; i++)
+        {
+            Rectangle r = new Rectangle
             {
-                Rectangle r = new Rectangle
-                {
-                    Width = ringWidth - i * (HelpClass.Difference),
-                    Height = HelpClass.RingHeight,
-                    Fill = HelpClass.ColorBrash(HelpClass.Colors.ColorsList[i])
-                };
-                Canvas.SetLeft(r, 120 - r.Width / 2);
-                Canvas.SetBottom(r, r.Height *i);
-                Col1.Children.Add(r);
-            }
+                Width = ringWidth - i * (HelpClass.Difference),
+                Height = HelpClass.RingHeight,
+                Fill = HelpClass.ColorBrash(HelpClass.Colors.ColorsList[i])
+            };
+            Canvas.SetLeft(r, 120 - r.Width / 2);
+            Canvas.SetBottom(r, r.Height *i);
+            Col1.Children.Add(r);
         }
+    }
 
 
-        private void RectangleCopy(Rectangle source, Rectangle copy, int sourceCol)
+    private void RectangleCopy(Rectangle source, Rectangle copy, int sourceCol)
+    {
+        copy.Fill = source.Fill;
+        copy.Width = source.Width;
+        copy.Height = source.Height;
+        
+        switch (sourceCol)
         {
-            copy.Fill = source.Fill;
-            copy.Width = source.Width;
-            copy.Height = source.Height;
+            case 0:
+                Canvas.SetLeft(copy, Canvas.GetLeft(source) + Canvas.GetLeft(Col1));
+                Canvas.SetBottom(copy, Canvas.GetBottom(source) + Canvas.GetBottom(Col1));
+                break;
+            case 1:
+                Canvas.SetLeft(copy, Canvas.GetLeft(source) + Canvas.GetLeft(Col2));
+                Canvas.SetBottom(copy, Canvas.GetBottom(source) + Canvas.GetBottom(Col2));
+                break;
+            case 2:
+                Canvas.SetLeft(copy, Canvas.GetLeft(source) + Canvas.GetLeft(Col3));
+                Canvas.SetBottom(copy, Canvas.GetBottom(source) + Canvas.GetBottom(Col3));
+                break;
+        }
+    }
+    private void Anima(Rectangle r, int to, DoubleAnimation leftAnimation, DoubleAnimation bottomAnimation)
+    { 
+        leftAnimation.From = Canvas.GetLeft(r);
+        bottomAnimation.From = Canvas.GetBottom(r);
 
-            switch (sourceCol)
-            {
-                case 0:
-                    Canvas.SetLeft(copy, Canvas.GetLeft(source) + Canvas.GetLeft(Col1));
-                    Canvas.SetBottom(copy, Canvas.GetBottom(source) + Canvas.GetBottom(Col1));
-                    break;
-                case 1:
-                    Canvas.SetLeft(copy, Canvas.GetLeft(source) + Canvas.GetLeft(Col2));
-                    Canvas.SetBottom(copy, Canvas.GetBottom(source) + Canvas.GetBottom(Col2));
-                    break;
-                case 2:
-                    Canvas.SetLeft(copy, Canvas.GetLeft(source) + Canvas.GetLeft(Col3));
-                    Canvas.SetBottom(copy, Canvas.GetBottom(source) + Canvas.GetBottom(Col3));
-                    break;
-            }
+        switch (to)
+        {
+            case 0:
+                leftAnimation.To = Canvas.GetLeft(Col1) + ((Col1.Width / 2) - (r.Width / 2));
+                bottomAnimation.To = Canvas.GetBottom(Col1) + (Col1.Children.Count * HelpClass.RingHeight);
+                break;
+            case 1:
+                leftAnimation.To = Canvas.GetLeft(Col2) + ((Col2.Width / 2) - r.Width / 2);
+                bottomAnimation.To = Canvas.GetBottom(Col1) + (Col2.Children.Count * HelpClass.RingHeight);
+                break;
+            case 2:
+                leftAnimation.To = Canvas.GetLeft(Col3) + (Col3.Width / 2 - r.Width / 2);
+                bottomAnimation.To = Canvas.GetBottom(Col1) + (Col3.Children.Count * HelpClass.RingHeight);
+                break;
+        }
+        leftAnimation.Duration = TimeSpan.FromSeconds((int)Slider.Value * 0.35);
+        bottomAnimation.Duration = TimeSpan.FromSeconds((int)Slider.Value * 0.35);
+    }
+    private async Task Move(int from,int to)
+    {
+        Canvas fromCol;
+        switch (from)
+        {
+            case 0:
+                fromCol = Col1;
+                break;
+            case 1:
+                fromCol = Col2;
+                break;
+            case 2:
+                fromCol = Col3;
+                break;
+            default:
+                fromCol = Col1;
+                break;
+        }
+        Canvas toCol;
+        switch (to)
+        {
+            case 0:
+                toCol = Col1;
+                break;
+            case 1:
+                toCol = Col2;
+                break;
+            case 2:
+                toCol = Col3;
+                break;
+            default:
+                toCol = Col1;
+                break;
+        }
+        DoubleAnimation leftAnimation = new DoubleAnimation();
+        DoubleAnimation bottomAnimation = new DoubleAnimation();
+
+        Rectangle copy = new Rectangle();
+        Rectangle r = (Rectangle)fromCol.Children[^1];
             
-        }
-        private void Anima(Rectangle r, int to, DoubleAnimation leftAnimation, DoubleAnimation bottomAnimation)
-        { 
-            leftAnimation.From = Canvas.GetLeft(r);
-            bottomAnimation.From = Canvas.GetBottom(r);
-
-            switch (to)
-            {
-                case 0:
-                    leftAnimation.To = Canvas.GetLeft(Col1) + ((Col1.Width / 2) - (r.Width / 2));
-                    bottomAnimation.To = Canvas.GetBottom(Col1) + (Col1.Children.Count * HelpClass.RingHeight);
-                    break;
-                case 1:
-                    leftAnimation.To = Canvas.GetLeft(Col2) + ((Col2.Width / 2) - r.Width / 2);
-                    bottomAnimation.To = Canvas.GetBottom(Col1) + (Col2.Children.Count * HelpClass.RingHeight);
-                    break;
-                case 2:
-                    leftAnimation.To = Canvas.GetLeft(Col3) + (Col3.Width / 2 - r.Width / 2);
-                    bottomAnimation.To = Canvas.GetBottom(Col1) + (Col3.Children.Count * HelpClass.RingHeight);
-                    break;
-            }
-            leftAnimation.Duration = TimeSpan.FromSeconds((int)Slider.Value * 0.35);
-             bottomAnimation.Duration = TimeSpan.FromSeconds((int)Slider.Value * 0.35);
-        }
-        private async Task Move(int from,int to)
+        RectangleCopy(r, copy, from);
+        Anima(copy, to, leftAnimation, bottomAnimation);
+        fromCol.Children.Remove(r);
+        MainCanvas.Children.Add(copy);
+        copy.BeginAnimation(Canvas.LeftProperty,leftAnimation);
+        copy.BeginAnimation(Canvas.BottomProperty, bottomAnimation);
+        Canvas.SetBottom(r, toCol.Children.Count * HelpClass.RingHeight);
+        await Task.Delay((int) (Slider.Value * 350));
+        toCol.Children.Add(r);
+        MainCanvas.Children.Remove(copy);
+    }
+    private void HanoiTower(int n, int from=0, int to=1, int aux=2)
+    {
+        if (n > 0)
         {
-            Canvas fromCol;
-            switch (from)
-            {
-                case 0:
-                    fromCol = Col1;
-                    break;
-                case 1:
-                    fromCol = Col2;
-                    break;
-                case 2:
-                    fromCol = Col3;
-                    break;
-                default:
-                    fromCol = Col1;
-                    break;
-            }
-            Canvas toCol;
-            switch (to)
-            {
-                case 0:
-                    toCol = Col1;
-                    break;
-                case 1:
-                    toCol = Col2;
-                    break;
-                case 2:
-                    toCol = Col3;
-                    break;
-                default:
-                    toCol = Col1;
-                    break;
-            }
-            DoubleAnimation leftAnimation = new DoubleAnimation();
-            DoubleAnimation bottomAnimation = new DoubleAnimation();
-
-            Rectangle copy = new Rectangle();
-            Rectangle r = (Rectangle)fromCol.Children[^1];
-            
-            RectangleCopy(r, copy, from);
-            Anima(copy, to, leftAnimation, bottomAnimation);
-            fromCol.Children.Remove(r);
-            MainCanvas.Children.Add(copy);
-            copy.BeginAnimation(Canvas.LeftProperty,leftAnimation);
-            copy.BeginAnimation(Canvas.BottomProperty, bottomAnimation);
-            Canvas.SetBottom(r, toCol.Children.Count * HelpClass.RingHeight);
-            await Task.Delay((int) (Slider.Value * 350));
-            toCol.Children.Add(r);
-            MainCanvas.Children.Remove(copy);
+            HanoiTower(n - 1, from, aux, to);
+            _movementsList.Add(new Tuple<int, int>(from, to));
+            HanoiTower(n - 1, aux, to, from);
         }
-        private void HanoiTower(int n, int from=0, int to=1, int aux=2)
+    }
+    private async void Start()
+    {
+        CreateField();
+        HanoiTower(_ringsCount);
+        foreach(var t in _movementsList)
         {
-            if (n > 0)
-            {
-                HanoiTower(n - 1, from, aux, to);
-                _movementsList.Add(new Tuple<int, int>(from, to));
-                HanoiTower(n - 1, aux, to, from);
-            }
+            await Move(t.Item1, t.Item2);
         }
-        private async void Start()
-        {
-            CreateField();
-            HanoiTower(_ringsCount);
-            foreach(var t in _movementsList)
-            {
-                await Move(t.Item1, t.Item2);
-            }
-        }
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            ((Slider)sender).SelectionEnd = e.NewValue;
-        }
+    }
+    private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        ((Slider)sender).SelectionEnd = e.NewValue;
     }
 }
